4260f2db52d3a90b50de518f061a7a3e73358060
Yassir-17 <anfalbla@gmail.com>
2024-10-28 02:39:38 +0400

    delete the web server part

--- hello.c
+++ hello.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <assert.h>
+#include <unistd.h> // For sleep function
 
 // Function to return the "Hello World" message
 const char* getHelloMessage() {
@@ -19,5 +20,11 @@ int main() {
 
     // Print the message
     printf("%s\n", getHelloMessage());
+
+    // Infinite loop to keep the program running
+    while (1) {
+        sleep(5); // Sleep for 5 seconds to avoid busy waiting
+    }
+
     return 0;
 }
